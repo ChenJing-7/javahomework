9889f1f21019b7f4fe9edbf95639055d93a98d57
ChenJing-7 <37971201+ChenJing-7@users.noreply.github.com>
2020-10-21 09:07:52 +0800

    Create Matrix.java

---
+++ Matrix.java
@@ -0,0 +1,27 @@
+package of.chenjing;
+
+import java.util.Scanner;
+public class Matrix {
+
+	public static void main(String[] args) {
+		System.out.println("请输入一个正整数");
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		printMatrix(n);
+
+	}
+	public static void printMatrix(int n){
+		
+		for(int i = 1;i <= (int)(Math.pow(n,2));i++){
+			double a = Math.random();
+			int b = (int)((a < 0.5)? 0 : 1);
+			if(i % n == 0){
+				System.out.println(b);
+			}else{
+				System.out.print(b + " ");
+			}
+		}
+				
+	}
+
+}
